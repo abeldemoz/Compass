57fdd029bc6e4d1952e142effd7a732254a7d47c
Abel Demoz <86599566+abeldemoz@users.noreply.github.com>
2024-03-29 20:18:38 +0000

    WIP5

---
+++ Sources/Compass/Implementations/BasicTabBarController.swift
@@ -0,0 +1,18 @@
+//
+// BasicTabBarController.swift
+//
+//
+// Created by Abel Demoz on 29/03/2024.
+//
+
+import UIKit
+
+public class BasicTabBarController: UITabBarController, TabBarController {
+
+    public var onDismissed: (() -> Void)? = nil
+
+    deinit {
+        onDismissed?()
+        onDismissed = nil
+    }
+}
